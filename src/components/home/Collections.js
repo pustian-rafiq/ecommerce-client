38766505bc750529c02b8b5d27863b66754f6bd8
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-05-27 16:12:44 +0600

    design special collection  product

---
+++ src/components/home/Collections.js
@@ -0,0 +1,214 @@
+import React, { Component } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export class Collections extends Component {
+  render() {
+    return (
+      <>
+        <Container className="text-center BetweenTwoSection" fluid={true}>
+          <h4 className="section-title ">SPECIAL COLLECTION</h4>
+          <h6 className="section-sub-title">
+            Some Of Our Exclusive Collection, You May Like
+          </h6>
+          <Row>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/b608415c3bdd4ab20195fa7dd088e2aa.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/mdc/c903bc953495801e4564e79d6b23ccbf.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/b608415c3bdd4ab20195fa7dd088e2aa.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/e62757e580dc37d455be8dc340b5a19b.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/0ca83d798be23f95fb50e2e2b50fbd21.png_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/b608415c3bdd4ab20195fa7dd088e2aa.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/da6c8149ebc3b017a7730a7a00521367.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/c14feee9f89e87cd0ae390c979a91743.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/b608415c3bdd4ab20195fa7dd088e2aa.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/8506b4eaef2193424adcba87e32076e2.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/b608415c3bdd4ab20195fa7dd088e2aa.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+            <Col className="p-1" xl={2} lg={2} md={2} sm={6} xs={6}>
+              <Link to="/">
+                <Card className="card h-100  text-center w-100  image-box ">
+                  <img
+                    src="https://static-01.daraz.com.bd/p/b608415c3bdd4ab20195fa7dd088e2aa.jpg_400x400q75-product.jpg_.webp"
+                    alt=""
+                  />
+                  <Card.Body>
+                    <h5 className="product-name-on-card">
+                      Mashroom LED Sensor Light
+                    </h5>
+                    <p className="product-price-on-card">Price: 300TK</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default Collections;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 //import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
 import Categories from "../components/home/Categories";
+import Collections from "../components/home/Collections";
 //import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import FeaturedProduct from "../components/home/FeaturedProduct";
 
@@ -10,6 +11,7 @@ export class HomePage extends Component {
       <>
         <FeaturedProduct />
         <Categories />
+        <Collections />
       </>
     );
   }
